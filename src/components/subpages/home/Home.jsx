f75c39d9b4f07757d12fd59c085bc3efe7e958cf
Michal Stasiak <michal.stasiak@protonmail.com>
2025-05-03 11:15:19 +0200

    add utlis for cap first letter

--- src/components/subpages/home/Home.jsx
+++ src/components/subpages/home/Home.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Home = () => {
+  const [pokemons, setPokemons] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      try {
+        const response = await axios.get(
+          "https://pokeapi.co/api/v2/pokemon?limit=150"
+        );
+        setPokemons(response.data.results);
+      } catch (err) {
+        setError("Błąd podczas pobierania danych.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPokemons();
+  }, []);
+
+  if (loading) return <p>Ładowanie...</p>;
+  if (error) return <p>{error}</p>;
+
+  console.log(pokemons[0]);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Lista Pokémonów</h1>
+      <ul>
+        {pokemons.map((pokemon, index) => (
+          <li key={index}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
